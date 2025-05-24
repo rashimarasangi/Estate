a9b5a939de9244cc8c85b8384fb05067422ee0b1
rashimarasangi <rashimarasangi200@gmail.com>
2025-05-24 21:22:55 +0530

    header section is added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,16 @@ const Navbar = () => {
             <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
+        <img src={assets.menu_icon} className='md:hidden w-7' alt="" />
+      </div>
+      {/* ---------mobile-menu----- */}
+      <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden'>
+        <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+          <a href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+          <a href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
+          <a href="#Projects" className='px-4 py-2 rounded-full inline-block'>projects</a>
+          <a href="#Testimonials" className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
+        </ul>
       </div>
     </div>
   )
